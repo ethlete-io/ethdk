33d9770c020dc70db1729ab2fd57b15aab39dd0b
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-01 13:38:58 +0000

    dev-registration from API (3b2eb23d)

--- libs/types/src/lib/api/Tournament/index.ts
+++ libs/types/src/lib/api/Tournament/index.ts
@@ -1,1 +1,7 @@
 export { TournamentListView } from './tournamentList.view';
+export { GroupStageStageStructureView } from './groupStageStageStructure.view';
+export { RoundStageStructureView } from './roundStageStructure.view';
+export { GroupStageStructureView } from './groupStageStructure.view';
+export { BaseStageStageStructureView } from './baseStageStageStructure.view';
+export { StageType } from './stage.type';
+export { RoundType } from './round.type';
