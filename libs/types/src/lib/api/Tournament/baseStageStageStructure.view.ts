0c8778c009e92bb8d237a5333775bd2eee69ad61
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-02 11:57:23 +0000

    dev-registration from API (dd3b258a)

---
+++ libs/types/src/lib/api/Tournament/baseStageStageStructure.view.ts
@@ -0,0 +1,11 @@
+// Generated Interface, do not change manually!
+import { StageType } from './stage.type';
+
+export interface BaseStageStageStructureView {
+    id: string;
+    status: string;
+    name: string;
+    type: StageType;
+}
+
+export default BaseStageStageStructureView;
---
+++ libs/types/src/lib/api/Tournament/groupStageStructure.view.ts
@@ -0,0 +1,11 @@
+// Generated Interface, do not change manually!
+import { RoundStageStructureView } from './roundStageStructure.view';
+
+export interface GroupStageStructureView {
+    id: string;
+    status: string;
+    name: string;
+    rounds: RoundStageStructureView[];
+}
+
+export default GroupStageStructureView;
