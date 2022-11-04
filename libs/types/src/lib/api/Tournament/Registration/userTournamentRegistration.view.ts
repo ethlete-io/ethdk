ec0c375641af2a5f50763fc108dfa6732b70a94d
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-04 16:52:25 +0000

    dev-registration from API (f59ad07c)

--- libs/types/src/lib/api/Tournament/Registration/userTournamentRegistration.view.ts
+++ libs/types/src/lib/api/Tournament/Registration/userTournamentRegistration.view.ts
@@ -1,10 +1,14 @@
 // Generated Interface, do not change manually!
+import { DateTime } from './date.time';
 
 export interface UserTournamentRegistrationView {
     id: string;
     tournamentId: string;
     userId: number;
-    state: string;
+    position: number;
+    state: string | null;
+    tournamentSize: number | null;
+    tournamentStartDate: DateTime | null;
 }
 
 export default UserTournamentRegistrationView;
