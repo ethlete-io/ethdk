fc677c844c6f1fe13d8285a4cf41e66d5793d1cd
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-12 12:43:55 +0000

    dev-bvb from API (3141bd8e)

---
+++ libs/types/src/lib/api/Season/seasonWithTournamentGroups.view.ts
@@ -0,0 +1,10 @@
+// Generated Interface, do not change manually!
+import { TournamentGroupListView } from './../TournamentGroup/tournamentGroupList.view';
+import { BaseSeasonView } from './baseSeason.view';
+
+export interface SeasonWithTournamentGroupsView {
+    season: BaseSeasonView;
+    tournamentGroups: TournamentGroupListView[];
+}
+
+export default SeasonWithTournamentGroupsView;
