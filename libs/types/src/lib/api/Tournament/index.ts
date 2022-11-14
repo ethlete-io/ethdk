bb35fd70fdbd65ea4838782cb81e908e6958e694
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-14 08:28:28 +0000

    dev-bvb from API (b5bc8d47)

---
+++ libs/types/src/lib/api/Tournament/index.ts
@@ -0,0 +1,9 @@
+export { BaseStageStageStructureViewUnion } from './baseStageStageStructureView.union';
+export { TournamentListView } from './tournamentList.view';
+export { SingleEliminationStageStageStructureView } from './singleEliminationStageStageStructure.view';
+export { DoubleEliminationStageStageStructureView } from './doubleEliminationStageStageStructure.view';
+export { GroupStageStageStructureView } from './groupStageStageStructure.view';
+export { ListSearchRequestData } from './listSearchRequest.data';
+export { RoundStageStructureView } from './roundStageStructure.view';
+export { GroupStageStructureView } from './groupStageStructure.view';
+export { BaseStageStageStructureView } from './baseStageStageStructure.view';
