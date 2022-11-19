90612d8083a41e2fb64db893791ab1f576d564c6
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2022-11-19 18:01:25 +0100

    feat: 🎸 Bring back DestoryService to pevent error NG0309

--- libs/core/src/lib/services/public-api.ts
+++ libs/core/src/lib/services/public-api.ts
@@ -1,5 +1,6 @@
 export * from './click-observer.service';
 export * from './content-observer.service';
+export * from './destroy.service';
 export * from './focus-visible.service';
 export * from './resize-observer.service';
 export * from './viewport.service';
