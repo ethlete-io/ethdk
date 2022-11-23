4f047ace69b4d35e648e87b61a37618fc63c2d63
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-23 16:05:23 +0000

    dev-bvb from API (4cacd0ff)

---
+++ libs/types/src/lib/api/Game/gameRanking.view.ts
@@ -0,0 +1,9 @@
+// Generated Interface, do not change manually!
+import { RankingStatus } from './../Enum/Ranking/ranking.status';
+
+export interface GameRankingView {
+    status: RankingStatus | null;
+    score: number | null;
+}
+
+export default GameRankingView;
---
+++ libs/types/src/lib/api/Match/Stream/matchListWithStream.view.ts
@@ -0,0 +1,10 @@
+// Generated Interface, do not change manually!
+import { MatchListView } from './../../Match/matchList.view';
+import { MatchStreamDetailView } from './matchStreamDetail.view';
+
+export interface MatchListWithStreamView {
+    match: MatchListView;
+    streams: MatchStreamDetailView[];
+}
+
+export default MatchListWithStreamView;
