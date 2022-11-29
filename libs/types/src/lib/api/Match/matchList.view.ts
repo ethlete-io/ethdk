c42d44d1150d149ba233420b393f596e8dcad2dc
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-29 08:18:20 +0000

    dev-bvb from API (bcc9fefe)

---
+++ libs/types/src/lib/api/Match/matchList.view.ts
@@ -0,0 +1,33 @@
+// Generated Interface, do not change manually!
+import { MatchStatus } from './../Enum/Match/match.status';
+import { MatchType } from './../Enum/Match/match.type';
+import { OpponentSide } from './../Enum/Opponent/opponent.side';
+import { StageType } from './../Enum/Stage/stage.type';
+import { RoundListView } from './../Round/roundList.view';
+import { TournamentListView } from './../Tournament/tournamentList.view';
+import { TournamentGroupListView } from './../TournamentGroup/tournamentGroupList.view';
+import { ParticipantViewUnion } from './../Participant/participantView.union';
+import { GameListViewUnion } from './../Game/gameListView.union';
+import { MatchRankingView } from './matchRanking.view';
+
+export interface MatchListView {
+    id: string;
+    status: MatchStatus | null;
+    number: number | null;
+    type: MatchType | null;
+    startTime: string;
+    home: ParticipantViewUnion | null;
+    away: ParticipantViewUnion | null;
+    games: GameListViewUnion[];
+    judged: boolean;
+    homeScore: MatchRankingView | null;
+    awayScore: MatchRankingView | null;
+    tournamentGroup: TournamentGroupListView | null;
+    tournament: TournamentListView | null;
+    round: RoundListView;
+    winningSide: OpponentSide | null;
+    matchType: StageType | null;
+    isCompletedByReferee: boolean;
+}
+
+export default MatchListView;
