b9d6bd5e928d599b9fee049d1eb8e8ad651ec9cc
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-29 16:19:27 +0000

    dev-bvb from API (231cc870)

---
+++ libs/types/src/lib/api/Country/country.view.ts
@@ -0,0 +1,11 @@
+// Generated Interface, do not change manually!
+import { MediaView } from './../Media/media.view';
+
+export interface CountryView {
+    id: string;
+    code: string | null;
+    shortName: string | null;
+    flag: MediaView | null;
+}
+
+export default CountryView;
