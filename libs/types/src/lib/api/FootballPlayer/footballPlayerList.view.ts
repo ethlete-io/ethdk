5eddc169ff7dd61e026222459a461123fdaadd38
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-29 15:16:09 +0000

    dev-bvb from API (28fa82ad)

---
+++ libs/types/src/lib/api/FootballPlayer/footballPlayerList.view.ts
@@ -0,0 +1,13 @@
+// Generated Interface, do not change manually!
+
+export interface FootballPlayerListView {
+    id: string;
+    type: string | null;
+    rating: number | null;
+    position: string | null;
+    clubName: string | null;
+    firstName: string;
+    lastName: string;
+}
+
+export default FootballPlayerListView;
