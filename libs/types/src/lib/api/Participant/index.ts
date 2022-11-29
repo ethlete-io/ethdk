6678785b4b1ce955b4c066e59e6fcd5f25340883
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-29 14:38:54 +0000

    master from API (a6beeb68)

---
+++ libs/types/src/lib/api/Participant/index.ts
@@ -0,0 +1,4 @@
+export { ParticipantViewUnion } from './participantView.union';
+export { PlayerParticipantView } from './playerParticipant.view';
+export { AbstractParticipantView } from './abstractParticipant.view';
+export { TeamParticipantView } from './teamParticipant.view';
