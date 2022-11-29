d5ce751cecc2ecf8e92530fb117d0619351a17f4
Gitlab CI Pipeline <gitlab@braune-digital.com>
2022-11-29 17:00:19 +0000

    dev-bvb from API (7f285aae)

---
+++ libs/types/src/lib/api/Platform/platformList.view.ts
@@ -0,0 +1,11 @@
+// Generated Interface, do not change manually!
+import { MediaView } from './../Media/media.view';
+
+export interface PlatformListView {
+    id: string;
+    title: string;
+    logo: MediaView | null;
+    identifier: string | null;
+}
+
+export default PlatformListView;
