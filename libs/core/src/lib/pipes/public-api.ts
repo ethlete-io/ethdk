e1e2f7405641a47cd608f375f1174d367538da89
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2022-12-08 13:12:18 +0100

    feat: 🎸 Add normalize game result type pipe

--- libs/core/src/lib/pipes/public-api.ts
+++ libs/core/src/lib/pipes/public-api.ts
@@ -1,3 +1,4 @@
+export * from './normalize-game-result-type/public-api';
 export * from './normalize-match-score/public-api';
 export * from './normalize-match-state/public-api';
 export * from './normalize-match-type/public-api';
