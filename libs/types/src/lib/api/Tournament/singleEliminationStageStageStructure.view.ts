8f7cd2403d5b40a32cda861c1f6a2b16fd4db6de
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-01-13 10:21:34 +0000

    dev-check-in from API (02e0e29e)

---
+++ libs/types/src/lib/api/Tournament/singleEliminationStageStageStructure.view.ts
@@ -0,0 +1,9 @@
+// Generated Interface, do not change manually!
+import { BaseStageStageStructureView } from './baseStageStageStructure.view';
+import { RoundStageStructureView } from './roundStageStructure.view';
+
+export interface SingleEliminationStageStageStructureView extends BaseStageStageStructureView {
+    rounds: RoundStageStructureView[];
+}
+
+export default SingleEliminationStageStageStructureView;
