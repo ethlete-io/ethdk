1d3a46ba37fff240355ad0012ea5b9e47c27eb7f
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-01-13 11:04:37 +0000

    master from API (f5c7d4cc)

---
+++ libs/types/src/lib/api/Tournament/listSearchRequest.data.ts
@@ -0,0 +1,14 @@
+// Generated Interface, do not change manually!
+import { DateRangeRequestData } from './../DateRange/dateRangeRequest.data';
+import { PaginatedSearchRequestData } from './../Request/paginatedSearchRequest.data';
+
+export interface ListSearchRequestData extends PaginatedSearchRequestData {
+    datePeriod: DateRangeRequestData;
+
+    /**
+     * @default []
+     */
+    status?: any;
+}
+
+export default ListSearchRequestData;
