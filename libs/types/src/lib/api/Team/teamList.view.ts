f2a11ffb1353e500866c43e15df14b52d1ce15db
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-01-13 09:56:02 +0000

    master from API (783f4b08)

---
+++ libs/types/src/lib/api/Team/teamList.view.ts
@@ -0,0 +1,9 @@
+// Generated Interface, do not change manually!
+import { MediaView } from './../Media/media.view';
+
+export interface TeamListView {
+    name: string;
+    image: MediaView | null;
+}
+
+export default TeamListView;
