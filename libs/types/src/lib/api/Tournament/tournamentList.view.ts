744f3355cae8e72d1e4b0f8724856bd0d80223b3
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-01-13 09:37:04 +0000

    master from API (9afbae23)

---
+++ libs/types/src/lib/api/Tournament/tournamentList.view.ts
@@ -0,0 +1,12 @@
+// Generated Interface, do not change manually!
+import { TournamentStatus } from './../Enum/Tournament/tournament.status';
+
+export interface TournamentListView {
+    id: string;
+    title: string | null;
+    shortTitle: string | null;
+    slug: string | null;
+    status: TournamentStatus | null;
+}
+
+export default TournamentListView;
