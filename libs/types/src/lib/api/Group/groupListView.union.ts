f20bc372929e4fb2a7f2a60394de560deae6803f
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-01-13 14:52:44 +0000

    master from API (cfbd8ec3)

---
+++ libs/types/src/lib/api/Group/groupListView.union.ts
@@ -0,0 +1,5 @@
+// Generated Type, do not change manually!
+import { GroupListView } from './../Group/groupList.view';
+import { GroupDetailView } from './../Group/groupDetail.view';
+
+export type GroupListViewUnion = GroupListView | GroupDetailView;
