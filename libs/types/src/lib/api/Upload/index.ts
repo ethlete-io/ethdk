b249d6dc286560d18a6c142885d4d6a8524d1e6a
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-01-19 08:44:50 +0000

    master from API (a34b80a8)

---
+++ libs/types/src/lib/api/Upload/index.ts
@@ -0,0 +1,1 @@
+export { UploadView } from './upload.view';
---
+++ libs/types/src/lib/api/Upload/upload.view.ts
@@ -0,0 +1,11 @@
+// Generated Interface, do not change manually!
+
+export interface UploadView {
+    id: string;
+    userId: number;
+    width: number;
+    height: number;
+    size: number;
+}
+
+export default UploadView;
