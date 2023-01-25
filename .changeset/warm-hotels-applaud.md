bb740155333084777c57ca4082b0a7d504c1b4ae
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2023-01-25 11:23:54 +0100

    fix: 🐛 Fix typedefs for TypedQueryList

---
+++ .changeset/warm-hotels-applaud.md
@@ -0,0 +1,5 @@
+---
+'@ethlete/core': patch
+---
+
+Fix typedefs for TypedQueryList
--- libs/core/src/lib/types/angular.types.ts
+++ libs/core/src/lib/types/angular.types.ts
@@ -12,16 +12,28 @@ export type NgClassType =
   | null
   | undefined;
 
-export class TypedQueryList<T> extends QueryList<T> {
+export class TypedQueryList<T> extends QueryList<T | undefined> {
+  override [Symbol.iterator]: () => Iterator<T> = () => {
+    return super[Symbol.iterator]() as Iterator<T>;
+  };
+
   override get changes(): Observable<TypedQueryList<T>> {
     return super.changes as Observable<TypedQueryList<T>>;
   }
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  override readonly first = super.first as T | undefined;
+  override toArray(): T[] {
+    return super.toArray() as T[];
+  }
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  override readonly last = super.last as T | undefined;
+  override forEach(fn: (item: T, index: number, array: T[]) => void): void {
+    super.forEach(fn as (item: T | undefined, index: number, array: (T | undefined)[]) => void);
+  }
+
+  override filter(fn: (item: T, index: number, array: T[]) => boolean): T[] {
+    return super.filter(fn as (item: T | undefined, index: number, array: (T | undefined)[]) => boolean) as T[];
+  }
+
+  override map<U>(fn: (item: T, index: number, array: T[]) => U): U[] {
+    return super.map(fn as (item: T | undefined, index: number, array: (T | undefined)[]) => U);
+  }
 }
