e55f055306dbeac226cb9d3e989d3bca2804bd3f
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2023-01-29 01:51:38 +0100

    feat: 🎸 Add some missing methods to typed query list

---
+++ .changeset/neat-crabs-look.md
@@ -0,0 +1,5 @@
+---
+'@ethlete/core': minor
+---
+
+Add some missing methods to typed query list
--- libs/core/src/lib/types/angular.types.ts
+++ libs/core/src/lib/types/angular.types.ts
@@ -36,4 +36,18 @@ export class TypedQueryList<T> extends QueryList<T | undefined> {
   override map<U>(fn: (item: T, index: number, array: T[]) => U): U[] {
     return super.map(fn as (item: T | undefined, index: number, array: (T | undefined)[]) => U);
   }
+
+  override reduce<U>(
+    fn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U,
+    initialValue: U,
+  ): U {
+    return super.reduce(
+      fn as (previousValue: U, currentValue: T | undefined, currentIndex: number, array: (T | undefined)[]) => U,
+      initialValue,
+    );
+  }
+
+  override some(fn: (value: T, index: number, array: T[]) => boolean): boolean {
+    return super.some(fn as (value: T | undefined, index: number, array: (T | undefined)[]) => boolean);
+  }
 }
