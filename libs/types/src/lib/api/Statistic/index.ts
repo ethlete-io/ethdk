57b7e92e9056621150f0fe1a364b208c0da44a1b
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-02-19 19:57:08 +0000

    dev-statistic-requests from API (5dcbe253)

---
+++ libs/types/src/lib/api/Statistic/index.ts
@@ -0,0 +1,3 @@
+export { StatisticListView } from './statisticList.view';
+export { StatisticValueView } from './statisticValue.view';
+export { GroupedStatisticListView } from './groupedStatisticList.view';
