9e13ee0f120b12e477a6ac910c94c85f25b0847c
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-02-24 08:22:13 +0000

    dev-statistic-requests from API (52e80193)

---
+++ libs/types/src/lib/api/Identity/identity.view.ts
@@ -0,0 +1,11 @@
+// Generated Interface, do not change manually!
+import { MediaView } from './../Media/media.view';
+
+export interface IdentityView {
+    name: string;
+    id: string;
+    type: string;
+    mediaView: MediaView | null;
+}
+
+export default IdentityView;
---
+++ libs/types/src/lib/api/Statistic/groupedStatisticList.view.ts
@@ -0,0 +1,9 @@
+// Generated Interface, do not change manually!
+import { StatisticListView } from './statisticList.view';
+
+export interface GroupedStatisticListView {
+    type: string;
+    statistic: StatisticListView;
+}
+
+export default GroupedStatisticListView;
