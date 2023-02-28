033c740e78a561fdf9652dc0b0e6fc84abe36dc8
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2023-03-01 00:54:05 +0100

    feat: 🎸 Add util to create a behavior subject that exposes a su

---
+++ .changeset/rotten-lobsters-own.md
@@ -0,0 +1,5 @@
+---
+'@ethlete/core': minor
+---
+
+Add util to create a behavior subject that exposes a subscriber count
--- libs/core/src/lib/utils/public-api.ts
+++ libs/core/src/lib/utils/public-api.ts
@@ -5,5 +5,6 @@ export * from './cookie.util';
 export * from './equal.util';
 export * from './media-query-observable.util';
 export * from './reactive-binding.util';
+export * from './rxjs.utils';
 export * from './scrollable.utils';
 export * from './viewport.util';
