20194abddcb053ae42a43aeff4166bbd40b71301
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-03-08 21:09:46 +0000

    dev-statistic-requests from API (b0e0aaa9)

---
+++ libs/types/src/lib/api/Registration/checkIn.status.ts
@@ -0,0 +1,3 @@
+// Generated Type, do not change manually!
+
+export type CheckInStatus = 'check_in_pending' | 'checked_in' | 'last_minute_checked_in' | 'missed_checked_in' | 'check_in_not_required';
