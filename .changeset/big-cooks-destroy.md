400405dfded6a2b64ff69ce6a4b745c306ea5da2
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2023-03-27 10:34:41 +0200

    feat: 🎸 Add option to supply a origin element to createFlipAnim

---
+++ .changeset/big-cooks-destroy.md
@@ -0,0 +1,5 @@
+---
+'@ethlete/core': minor
+---
+
+Add option to supply a origin element to createFlipAnimation
--- libs/core/src/lib/utils/animation.utils.ts
+++ libs/core/src/lib/utils/animation.utils.ts
@@ -19,10 +19,34 @@ export const forceReflow = (element: HTMLElement = document.body) => {
   return element.offsetHeight;
 };
 
-export const createFlipAnimationGroup = (config: { elements: HTMLElement[]; duration?: number; easing?: string }) => {
+interface FlipAnimationGroupConfig {
+  /**
+   * The elements to animate
+   */
+  elements: Array<HTMLElement | { element: HTMLElement; originElement?: HTMLElement }>;
+
+  /**
+   * The duration of the animation in milliseconds
+   * @default 250
+   */
+  duration?: number;
+
+  /**
+   * The easing function to use for the animation
+   * @default 'cubic-bezier(0.4, 0, 0.2, 1)'
+   */
+  easing?: string;
+}
+
+export const createFlipAnimationGroup = (config: FlipAnimationGroupConfig) => {
   const { elements, duration = 250, easing = 'cubic-bezier(0.4, 0, 0.2, 1)' } = config;
 
-  const flips = elements.map((el) => createFlipAnimation({ element: el, duration, easing }));
+  const flips = elements.map((el) => {
+    const element = 'element' in el ? el.element : el;
+    const originElement = 'originElement' in el ? el.originElement : undefined;
+
+    return createFlipAnimation({ element, originElement, duration, easing });
+  });
 
   const onStart$ = combineLatest(flips.map((animation) => animation.onStart$));
   const onFinish$ = combineLatest(flips.map((animation) => animation.onFinish$));
@@ -50,10 +74,35 @@ export const createFlipAnimationGroup = (config: { elements: HTMLElement[]; dura
   };
 };
 
-export const createFlipAnimation = (config: { element: HTMLElement; duration?: number; easing?: string }) => {
-  const { element: el, duration = 250, easing = 'cubic-bezier(0.4, 0, 0.2, 1)' } = config;
+export interface FlipAnimationConfig {
+  /**
+   * The element to animate
+   */
+  element: HTMLElement;
 
-  let initialRect = el.getBoundingClientRect();
+  /**
+   * The element to use as the origin for the animation
+   * @default element
+   */
+  originElement?: HTMLElement;
+
+  /**
+   * The duration of the animation in milliseconds
+   * @default 250
+   */
+  duration?: number;
+
+  /**
+   * The easing function to use for the animation
+   * @default 'cubic-bezier(0.4, 0, 0.2, 1)'
+   */
+  easing?: string;
+}
+
+export const createFlipAnimation = (config: FlipAnimationConfig) => {
+  const { element: el, originElement = el, duration = 250, easing = 'cubic-bezier(0.4, 0, 0.2, 1)' } = config;
+
+  let initialRect = originElement.getBoundingClientRect();
   let animation: Animation | null = null;
 
   const onStart$ = new Subject<void>();
@@ -80,7 +129,7 @@ export const createFlipAnimation = (config: { element: HTMLElement; duration?: n
   };
 
   const updateInit = () => {
-    initialRect = el.getBoundingClientRect();
+    initialRect = originElement.getBoundingClientRect();
   };
 
   const play = () => {
