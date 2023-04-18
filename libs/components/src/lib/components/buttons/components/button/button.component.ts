ec8759f1782e11f473c50dbd5ab19bb08431e1c3
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2023-04-19 00:03:17 +0200

    feat: 🎸 wip - button components

--- libs/components/src/lib/components/buttons/components/button/button.component.ts
+++ libs/components/src/lib/components/buttons/components/button/button.component.ts
@@ -1,18 +1,18 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation, inject } from '@angular/core';
-import { ButtonDirective } from '../../directives';
+import { ButtonDirective } from '@ethlete/cdk';
 
 @Component({
-  selector: '[et-button]',
+  selector: '[et-elevated-button], [et-filled-button], [et-tonal-button], [et-outlined-button], [et-text-button]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
-  hostDirectives: [{ directive: ButtonDirective, inputs: ['disabled', 'type', 'pressed'] }],
   host: {
     class: 'et-button',
   },
+  hostDirectives: [{ directive: ButtonDirective, inputs: ['disabled', 'type', 'pressed'] }],
 })
 export class ButtonComponent {
-  protected button = inject(ButtonDirective);
+  protected readonly button = inject(ButtonDirective);
 }
