83f669aca4fff6a05491a3cefb4031eafa34b65e
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2023-06-22 10:00:22 +0200

    feat: 🎸 Add createMutationObservable util

--- libs/core/src/lib/utils/public-api.ts
+++ libs/core/src/lib/utils/public-api.ts
@@ -6,6 +6,7 @@ export * from './cookie.util';
 export * from './destroy.utils';
 export * from './equal.util';
 export * from './media-query-observable.util';
+export * from './mutation-observable.util';
 export * from './reactive-binding.util';
 export * from './round.utils';
 export * from './runtime-error.utils';
