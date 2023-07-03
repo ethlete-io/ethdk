697e5e35cc6c26161b8079bb0d90d3664e8d8d92
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-07-03 14:48:01 +0000

    master from API (ab431335)

---
+++ libs/types/src/lib/api/Participant/teamParticipantDetail.view.ts
@@ -0,0 +1,8 @@
+// Generated Interface, do not change manually!
+import { TeamParticipantView } from './teamParticipant.view';
+
+export interface TeamParticipantDetailView extends TeamParticipantView {
+    roster: any;
+}
+
+export default TeamParticipantDetailView;
