a3f4ffdf3a902717ca9ed59e9184e442a0db1a70
Gw3i <contact.johngoldco@gmail.com>
2023-08-02 12:17:15 +0200

    refactor: 💡 Optimize mustMatch validation

--- libs/core/src/lib/validators/must-match.validator.ts
+++ libs/core/src/lib/validators/must-match.validator.ts
@@ -1,22 +1,32 @@
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export const MUST_MATCH = 'mustMatch';
 
-export const MustMatch = (controlName: string, matchingControlName: string) => {
-  return (formGroup: FormGroup) => {
-    const control = formGroup.controls[controlName];
-    const matchingControl = formGroup.controls[matchingControlName];
-
-    // set error on matchingControl if validation fails
-    if (matchingControl.errors && !matchingControl.errors[MUST_MATCH]) {
-      return;
+export const MustMatch = (controlName: string, matchingControlName: string): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const formGroup = control;
+    if (!formGroup) {
+      return { passwordConfirm: true };
     }
 
     // set error on matchingControl if validation fails
-    if (control.value !== matchingControl.value) {
-      matchingControl.setErrors({ [MUST_MATCH]: true });
-    } else {
-      matchingControl.setErrors(null);
+    if (formGroup.errors && !formGroup.errors[MUST_MATCH]) {
+      return null;
     }
+
+    const password = formGroup.get(controlName);
+    const passwordConfirm = formGroup.get(matchingControlName);
+
+    if (!password || !passwordConfirm) {
+      return { passwordConfirm: true };
+    }
+
+    // set error on matchingControl if validation fails
+    if (password.value !== passwordConfirm.value) {
+      passwordConfirm.setErrors({ passwordConfirm: true });
+      return { passwordConfirm: true };
+    }
+
+    return null;
   };
 };
