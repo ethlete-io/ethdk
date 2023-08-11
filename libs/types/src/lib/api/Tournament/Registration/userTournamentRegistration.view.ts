067a0fe5262545503b04c93390518f46ef03002a
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-08-11 08:54:51 +0000

    master from API (f2831d9e)

--- libs/types/src/lib/api/Tournament/Registration/userTournamentRegistration.view.ts
+++ libs/types/src/lib/api/Tournament/Registration/userTournamentRegistration.view.ts
@@ -1,11 +1,17 @@
 // Generated Interface, do not change manually!
+import { CheckInStatus } from './../../Registration/checkIn.status';
 
 export interface UserTournamentRegistrationView {
     id: string;
     tournamentId: string;
     userId: number;
     position: number;
-    state: string | null;
+    checkInStatus: CheckInStatus;
+    checkInActive: boolean;
+    checkInStartAt: string | null;
+    checkInEndAt: string | null;
+    lastMinuteCheckInStartAt: string | null;
+    lastMinuteCheckInEndAt: string | null;
     tournamentSize: number | null;
     tournamentStartDate: string | null;
     tournament: any;
