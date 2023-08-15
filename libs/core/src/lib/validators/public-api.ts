0ba88b49f2ffff30d4c70d95a3bd2f766ee7ef25
Vladyslav Nyzhashchyy <101993818+Gw3i@users.noreply.github.com>
2023-08-15 23:08:35 +0200

    Feature/is at least one requiered validation (#861)
    * feat: 🎸 Add validateAtLeastOneRequired validator
    
    * docs: ✏️ Add changelog
    
    * refactor: 💡 Export validator correctly & add to ErrorService

--- libs/core/src/lib/validators/public-api.ts
+++ libs/core/src/lib/validators/public-api.ts
@@ -1,7 +1,9 @@
+import { ValidateAtLeastOneRequired } from './at-least-one-required.validator';
 import { IsArrayNotEmpty } from './is-array-not-empty.validator';
 import { IsEmail } from './is-email.validator';
 import { MustMatch } from './must-match.validator';
 
+export * from './at-least-one-required.validator';
 export * from './is-array-not-empty.validator';
 export * from './is-email.validator';
 export * from './must-match.validator';
@@ -10,4 +12,5 @@ export const Validators = {
   MustMatch,
   IsEmail,
   IsArrayNotEmpty,
+  ValidateAtLeastOneRequired,
 } as const;
