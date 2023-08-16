ce285644202e92b4389a3d0d9166c5f822e7bcbb
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2023-08-16 10:28:04 +0200

    fix: 🐛 Cleanup only release

---
+++ .changeset/plenty-berries-raise.md
@@ -0,0 +1,5 @@
+---
+'@ethlete/cdk': patch
+---
+
+Cleanup only release
--- libs/cdk/src/lib/components/forms/components/selection-list/directives/selection-list-option/selection-list-option.directive.ts
+++ libs/cdk/src/lib/components/forms/components/selection-list/directives/selection-list-option/selection-list-option.directive.ts
@@ -66,8 +66,6 @@ export class SelectionListOptionDirective {
   }
 
   getLabel() {
-    console.log(this.elementRef.nativeElement.textContent);
-
     return this.elementRef.nativeElement.textContent || '';
   }
 
