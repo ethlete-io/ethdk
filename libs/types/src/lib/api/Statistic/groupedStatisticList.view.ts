0c3345a3de358f484d28607700049b297d5d8dfa
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-09-06 08:30:39 +0000

    dev-vbl from API (69ad5ffa)

--- libs/types/src/lib/api/Statistic/groupedStatisticList.view.ts
+++
@@ -1,9 +0,0 @@
-// Generated Interface, do not change manually!
-import { StatisticListView } from './statisticList.view';
-
-export interface GroupedStatisticListView {
-    type: string;
-    statistic: StatisticListView;
-}
-
-export default GroupedStatisticListView;
