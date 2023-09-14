58227c3a31a72bf41fa487bee142415441545de0
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-09-14 12:06:00 +0000

    master from API (c714f010)

---
+++ libs/types/src/lib/api/Player/playerRoster.view.ts
@@ -0,0 +1,9 @@
+// Generated Interface, do not change manually!
+import { PlayerListView } from './playerList.view';
+import { MediaView } from './../Media/media.view';
+
+export interface PlayerRosterView extends PlayerListView {
+    image: MediaView | null;
+}
+
+export default PlayerRosterView;
--- libs/types/src/lib/api/Tournament/Registration/userTournamentRegistration.view.ts
+++ libs/types/src/lib/api/Tournament/Registration/userTournamentRegistration.view.ts
@@ -1,13 +1,22 @@
 // Generated Interface, do not change manually!
+import { CheckInStatus } from './../../Registration/checkIn.status';
+import { MediaView } from './media.view';
 
 export interface UserTournamentRegistrationView {
     id: string;
     tournamentId: string;
     userId: number;
     position: number;
-    state: string | null;
+    checkInStatus: CheckInStatus;
+    checkInActive: boolean;
+    checkInStartAt: string | null;
+    checkInEndAt: string | null;
+    lastMinuteCheckInStartAt: string | null;
+    lastMinuteCheckInEndAt: string | null;
     tournamentSize: number | null;
     tournamentStartDate: string | null;
+    name: string | null;
+    teamLogo: MediaView | null;
     tournament: any;
     tournamentGroup: any | null;
 }
