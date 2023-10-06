b0b1795900d3c190b19b015dd2faa6b99c92683d
Marc Gruner <mg@braune-digital.com>
2023-10-06 11:39:12 +0200

    refactor: 💡 remove console warning if no type is provided

--- libs/cdk/src/lib/components/picture/picture.component.ts
+++ libs/cdk/src/lib/components/picture/picture.component.ts
@@ -1,13 +1,5 @@
 import { NgClass, NgForOf, NgIf } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  TrackByFunction,
-  ViewEncapsulation,
-  inject,
-  isDevMode,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, TrackByFunction, ViewEncapsulation, inject } from '@angular/core';
 import { LetDirective } from '@ethlete/core';
 import { PictureDataDirective } from './picture-data.directive';
 import { PictureSource } from './picture.component.types';
@@ -53,10 +45,6 @@ export class PictureComponent {
   protected trackBySrc: TrackByFunction<PictureSource> = (_, item) => item.srcset;
 
   protected combineWithConfig(src: PictureSource) {
-    if (isDevMode() && src.type === '') {
-      console.warn(`The type attribute is missing for the following source`, src.srcset, this);
-    }
-
     if (!this.config?.baseUrl || src.srcset.startsWith('http')) {
       return src;
     }
