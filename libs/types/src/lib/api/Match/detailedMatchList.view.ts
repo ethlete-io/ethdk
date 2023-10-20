d1780fad5cbb579e6b038296cccc42b6cfec9220
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-10-20 19:48:06 +0000

    dev-vbl from API (2f031088)

--- libs/types/src/lib/api/Match/detailedMatchList.view.ts
+++
@@ -1,9 +0,0 @@
-// Generated Interface, do not change manually!
-import { MatchListView } from './matchList.view';
-
-export interface DetailedMatchListView extends MatchListView {
-    stageId: string | null;
-    groupId: string | null;
-}
-
-export default DetailedMatchListView;
--- libs/types/src/lib/api/Participant/abstractParticipant.view.ts
+++ libs/types/src/lib/api/Participant/abstractParticipant.view.ts
@@ -1,7 +1,6 @@
 // Generated Interface, do not change manually!
 import { CountryView } from './../Country/country.view';
 import { ParticipantType } from './../Enum/Participant/participant.type';
-import { IdentityView } from './../Identity/identity.view';
 import { MediaView } from './../Media/media.view';
 
 export interface AbstractParticipantView {
@@ -13,7 +12,6 @@ export interface AbstractParticipantView {
     emblem: MediaView | null;
     countryOfResidence: CountryView | null;
     countryOfRepresentation: CountryView | null;
-    identity: IdentityView | null;
 }
 
 export default AbstractParticipantView;
