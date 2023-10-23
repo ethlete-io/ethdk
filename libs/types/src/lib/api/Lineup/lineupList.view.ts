fc3c3835ed96f63285363f3b6ba5f8b1530add73
Nils Ziermann <github@nilsziermann.de>
2023-10-23 22:09:09 +0200

    master from API (052a5b5e) (#1095)
    Co-authored-by: Gitlab CI Pipeline <gitlab@braune-digital.com>

--- libs/types/src/lib/api/Lineup/lineupList.view.ts
+++ libs/types/src/lib/api/Lineup/lineupList.view.ts
@@ -1,10 +1,11 @@
 // Generated Interface, do not change manually!
+import { FormationStructureView } from './formationStructure.view';
 import { LineupPlayerView } from './lineupPlayer.view';
 
 export interface LineupListView {
     id: string;
     name: string;
-    formation: string[];
+    formation: FormationStructureView[];
     players: LineupPlayerView[];
 }
 
