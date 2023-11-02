7ab1fdbf5d4fded7cd275b193a1c3f5cae548eb9
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-11-02 17:15:55 +0000

    dev-vbl-new from API (caedf77a)

--- libs/types/src/lib/api/Group/groupRanking.view.ts
+++ libs/types/src/lib/api/Group/groupRanking.view.ts
@@ -1,10 +1,12 @@
 // Generated Interface, do not change manually!
 import { PlacementView } from './../Ranking/Placement/placement.view';
+import { RoundListView } from './../Round/roundList.view';
 
 export interface GroupRankingView {
     groupName: string;
     qualifiedPlayers: number | null;
     placements: PlacementView[];
+    currentRound: RoundListView | null;
 }
 
 export default GroupRankingView;
