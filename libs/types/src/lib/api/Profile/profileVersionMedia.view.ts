afd235d04c58185e48cbeb40ee293f363ff07b87
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-11-02 15:47:10 +0000

    dev-vbl-new from API (d37ef569)

---
+++ libs/types/src/lib/api/Profile/profileVersionMedia.view.ts
@@ -0,0 +1,10 @@
+// Generated Interface, do not change manually!
+import { MediaView } from './../Media/media.view';
+
+export interface ProfileVersionMediaView {
+    id: string;
+    key: string;
+    media: MediaView;
+}
+
+export default ProfileVersionMediaView;
