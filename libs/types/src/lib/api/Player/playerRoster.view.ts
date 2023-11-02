77e7bd762fdef48a1bea65baff35f40c3359318f
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-11-02 16:30:56 +0000

    dev-vbl-new from API (c2d8b81f)

--- libs/types/src/lib/api/Player/playerRoster.view.ts
+++ libs/types/src/lib/api/Player/playerRoster.view.ts
@@ -1,9 +1,7 @@
 // Generated Interface, do not change manually!
 import { PlayerListView } from './playerList.view';
-import { MediaView } from './../Media/media.view';
 
 export interface PlayerRosterView extends PlayerListView {
-    image: MediaView | null;
 }
 
 export default PlayerRosterView;
