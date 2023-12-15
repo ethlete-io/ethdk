13f552dcf90ee490dac6eda7a192fdccc9687e01
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-12-15 23:26:38 +0000

    dev-vbl-statistic from API (42ce4491)

--- libs/types/src/lib/api/Overall/index.ts
+++ libs/types/src/lib/api/Overall/index.ts
@@ -1,2 +1,3 @@
+export { StatisticHighestVictoryView } from './statisticHighestVictory.view';
 export { StatisticOverallTournamentView } from './statisticOverallTournament.view';
 export { StatisticOverallUtCardResultsView } from './statisticOverallUtCardResults.view';
---
+++ libs/types/src/lib/api/Overall/statisticHighestVictory.view.ts
@@ -0,0 +1,11 @@
+// Generated Interface, do not change manually!
+
+export interface StatisticHighestVictoryView {
+    matchId: string;
+    homeScore: number;
+    awayScore: number;
+    homeSidePlayers: any;
+    awaySidePlayers: any;
+}
+
+export default StatisticHighestVictoryView;
