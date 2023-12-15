ac8b65ee90232addbc7a6d4f1755dd3c84b79949
Gitlab CI Pipeline <gitlab@braune-digital.com>
2023-12-15 16:45:07 +0000

    dev-vbl-statistic from API (8b6eb9b2)

---
+++ libs/types/src/lib/api/Statistic/HeadToHead/statisticClub.view.ts
@@ -0,0 +1,22 @@
+// Generated Interface, do not change manually!
+import { TeamParticipantViewUnion } from './../../Participant/teamParticipantView.union';
+
+export interface StatisticClubView {
+    id: string;
+    statisticConfigurationId: string;
+    identityId: string;
+    participant: TeamParticipantViewUnion;
+    matchesPlayed: number;
+    matchWonSum: number | null;
+    matchLostSum: number | null;
+    matchTiedSum: number | null;
+    scoreSum: number | null;
+    averagePointsPerMatch: number | null;
+    averagePointsOneVsOnePerGame: number | null;
+    averagePointsTwoVsTwoPerGame: number | null;
+    placement: number | null;
+    efficiency: number | null;
+    createdAt: string;
+}
+
+export default StatisticClubView;
