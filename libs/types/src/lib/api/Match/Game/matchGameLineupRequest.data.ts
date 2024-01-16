19abb785391968c3404e240fa71878ac0786fb3b
Gitlab CI Pipeline <gitlab@braune-digital.com>
2024-01-16 19:48:36 +0000

    dev-vbl-statistic from API (5927db44)

---
+++ libs/types/src/lib/api/Match/Game/matchGameLineupRequest.data.ts
@@ -0,0 +1,11 @@
+// Generated Interface, do not change manually!
+
+export interface MatchGameLineupRequestData {
+
+    /**
+     * @default false
+     */
+    switchLineups?: boolean;
+}
+
+export default MatchGameLineupRequestData;
