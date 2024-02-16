c42a3ce083dad4e16fb62395e4313a14095745fa
Gitlab CI Pipeline <gitlab@braune-digital.com>
2024-02-16 12:53:52 +0000

    dev-vbl-statistic from API (c8b2b0f6)

---
+++ libs/types/src/lib/api/Overall/statisticOverallTournament.view.ts
@@ -0,0 +1,9 @@
+// Generated Interface, do not change manually!
+
+export interface StatisticOverallTournamentView {
+    totalMatchGameCount: number;
+    totalScoreSum: number;
+    averageScore: number;
+}
+
+export default StatisticOverallTournamentView;
