ccf7cb0464df718bac4477e1fb6cb43972f2c321
Gitlab CI Pipeline <gitlab@braune-digital.com>
2024-02-28 16:04:02 +0000

    master from API (9731088a)

--- libs/types/src/lib/api/Game/gameList.view.ts
+++ libs/types/src/lib/api/Game/gameList.view.ts
@@ -16,7 +16,6 @@ export interface GameListView {
     gameType: string | null;
     platform: PlatformListView | null;
     isActive: boolean;
-    matchGameNumber: number;
 }
 
 export default GameListView;
