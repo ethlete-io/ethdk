f744c9fb31f4807608834de8d2f6cd424208b177
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2024-05-15 10:51:43 +0200

    feat: 🎸 contentful gql common types

---
+++ .changeset/eleven-spoons-work.md
@@ -0,0 +1,5 @@
+---
+"@ethlete/contentful": minor
+---
+
+Add `ContentfulGqlCollectionFilterVariables`, `ContentfulGqlOrder` and `ContentfulGqlWhereFilter` types
--- libs/contentful/src/lib/gql/asset.fragments.ts
+++ libs/contentful/src/lib/gql/asset.fragments.ts
@@ -32,3 +32,77 @@ export interface ContentfulGqlAsset {
 export const isContentfulGqlAsset = (asset: any): asset is ContentfulGqlAsset => {
   return asset?.sys?.id && asset?.url && typeof asset?.size === 'number';
 };
+
+export type ContentfulGqlWhereFilter<TLinkedFrom extends Record<string, unknown> = Record<string, unknown>> = {
+  /**
+   * Filer by ids.
+   * @see https://www.contentful.com/developers/docs/references/graphql/#/reference/collection-filters/sys-filters
+   */
+  sys?: {
+    id?: string;
+    id_not?: string;
+    id_in?: string[];
+    id_not_in?: string[];
+    id_contains?: string;
+    id_not_contains?: string;
+  };
+  /**
+   * Filer by tags.
+   * @see https://www.contentful.com/developers/docs/references/graphql/#/reference/collection-filters/contentfulmetadata-filters
+   */
+  contentfulMetadata?: {
+    tags_exists?: boolean;
+    tags?: {
+      id_contains_some?: string[];
+      id_contains_none?: string[];
+      id_contains_all?: string[];
+    };
+  };
+  /**
+   * Retrieve a collection of entries linked to the specified entry or asset.
+   * @see https://www.contentful.com/developers/docs/references/graphql/#links-to-a-specific-item
+   */
+  linkedFrom?: TLinkedFrom;
+};
+
+export type ContentfulGqlOrder = `${string}_${'ASC' | 'DESC'}`;
+
+export type ContentfulGqlCollectionFilterVariables<
+  TCustomWhere extends Record<string, unknown> = Record<string, unknown>,
+  TLinkedFrom extends Record<string, unknown> = Record<string, unknown>,
+> = {
+  /**
+   * Zero-indexed offset in the collection from which items are fetched
+   * @default 0
+   */
+  skip?: number;
+
+  /**
+   * Maximum number of items to fetch. Maximum is 1000.
+   * @default 100
+   */
+  limit?: number;
+
+  /**
+   * Filter specifications to apply on the collection query.
+   * @see https://www.contentful.com/developers/docs/references/graphql/#/reference/collection-filters
+   */
+  where?: TCustomWhere & ContentfulGqlWhereFilter<TLinkedFrom>;
+
+  /**
+   * Order specifications to apply on the collection query.
+   * @see https://www.contentful.com/developers/docs/references/graphql/#/reference/collection-order
+   */
+  order?: ContentfulGqlOrder[];
+
+  /**
+   * When set to `true` the field will be resolved with non published content.
+   * @default false
+   */
+  preview?: boolean;
+
+  /**
+   * Locale for the collection items. If not set the default locale is used.
+   */
+  locale?: string;
+};
