005bf9e0e1bccd7e92071e148d34635e7dad8bf8
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2025-01-10 14:10:18 +0100

    fix: 🐛 garbage collection

--- libs/query/src/lib/experimental/query-dependencies.ts
+++ libs/query/src/lib/experimental/query-dependencies.ts
@@ -7,8 +7,16 @@ export type SetupQueryDependenciesOptions = {
 };
 
 export type QueryDependencies = {
+  /** The destroy ref of the query. */
   destroyRef: DestroyRef;
+
+  /** The destroy ref of the scope the query is in (e.g. a component) */
+  scopeDestroyRef: DestroyRef;
+
+  /** The query client */
   client: QueryClient;
+
+  /** The injector of the query */
   injector: EnvironmentInjector;
 };
 
@@ -16,13 +24,33 @@ export const setupQueryDependencies = (options: SetupQueryDependenciesOptions) =
   const environmentInjector = inject(EnvironmentInjector);
   const queryEnvironmentInjector = createEnvironmentInjector([], environmentInjector);
   const destroyRef = queryEnvironmentInjector.get(DestroyRef);
+  const scopeDestroyRef = inject(DestroyRef);
   const client = inject(options.clientConfig.token);
 
   const dependencies: QueryDependencies = {
     destroyRef,
+    scopeDestroyRef,
     client,
     injector: queryEnvironmentInjector,
   };
+
+  // cleanup the environment injector when the scope (e.g. the component the query is in) is destroyed
+  const scopeDestroyListener = scopeDestroyRef.onDestroy(() => {
+    try {
+      queryEnvironmentInjector.destroy();
+    } catch {
+      // ignore
+    }
+  });
+
+  destroyRef.onDestroy(() => {
+    // cleanup the scope destroy listener
+    try {
+      scopeDestroyListener();
+    } catch {
+      // ignore
+    }
+  });
 
   return dependencies;
 };
