46d1dd3e0f083cf49071bed3ed2c497968ea79a5
Gw3i <contact.johngoldco@gmail.com>
2025-03-12 15:41:32 +0100

    Export slider-thumb-content-template directive correctly

--- libs/cdk/src/lib/components/forms/public-api/index.ts
+++ libs/cdk/src/lib/components/forms/public-api/index.ts
@@ -106,6 +106,7 @@ export * from '../components/slide-toggle/slide-toggle.imports';
 
 export * from '../components/slider/components/slider';
 export * from '../components/slider/components/slider-field';
+export * from '../components/slider/directives/slider-thumb-content-template';
 export * from '../components/slider/slider.imports';
 
 export * from '../directives/dynamic-form-field';
