3355fb7e54c8c50382504fc5b7e19ee33ec28e1e
Gitlab CI Pipeline <gitlab@braune-digital.com>
2025-03-31 18:27:38 +0000

    dev-statistic-icon-league from API (2ac0d66f)

--- libs/types/src/lib/api/Tournament/index.ts
+++ libs/types/src/lib/api/Tournament/index.ts
@@ -1,11 +1,11 @@
 export { BaseStageStageStructureViewUnion } from './baseStageStageStructureView.union';
+export { BaseStageStageStructureView } from './baseStageStageStructure.view';
 export { TournamentListView } from './tournamentList.view';
 export { SingleEliminationStageStageStructureView } from './singleEliminationStageStageStructure.view';
-export { DoubleEliminationStageStageStructureView } from './doubleEliminationStageStageStructure.view';
 export { GroupStageStageStructureView } from './groupStageStageStructure.view';
+export { SwissStageStructureView } from './swissStageStructure.view';
+export { GroupStageStructureView } from './groupStageStructure.view';
 export { ListSearchRequestData } from './listSearchRequest.data';
 export { RoundStageStructureView } from './roundStageStructure.view';
-export { GroupStageStructureView } from './groupStageStructure.view';
-export { BaseStageStageStructureView } from './baseStageStageStructure.view';
-export { SwissStageStructureView } from './swissStageStructure.view';
+export { DoubleEliminationStageStageStructureView } from './doubleEliminationStageStageStructure.view';
 export { TotalUserRegistrationsView } from './totalUserRegistrations.view';
