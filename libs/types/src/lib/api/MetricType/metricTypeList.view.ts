244df5170b126247d1ee3955912bd9127c24e89c
Gitlab CI Pipeline <gitlab@braune-digital.com>
2025-07-28 12:29:01 +0000

    dev-statistic-icon-league from API (d9eb7a04)

---
+++ libs/types/src/lib/api/MetricType/metricTypeList.view.ts
@@ -0,0 +1,10 @@
+// Generated Interface, do not change manually!
+
+export interface MetricTypeListView {
+    identifier: string;
+    name: string;
+    calculation: string;
+    position: number;
+}
+
+export default MetricTypeListView;
