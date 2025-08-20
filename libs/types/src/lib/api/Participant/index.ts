0741faea14657049d010f376ac1380b9a6271501
Gitlab CI Pipeline <gitlab@braune-digital.com>
2025-08-20 16:34:26 +0000

    dev-fifagg from API (d7a9fc73)

--- libs/types/src/lib/api/Participant/index.ts
+++ libs/types/src/lib/api/Participant/index.ts
@@ -2,6 +2,6 @@ export { ParticipantViewUnion } from './participantView.union';
 export { TeamParticipantViewUnion } from './teamParticipantView.union';
 export { PlayerParticipantView } from './playerParticipant.view';
 export { AbstractParticipantView } from './abstractParticipant.view';
-export { TeamParticipantDetailView } from './teamParticipantDetail.view';
 export { TeamParticipantView } from './teamParticipant.view';
+export { TeamParticipantDetailView } from './teamParticipantDetail.view';
 export { ParticipantListView } from './participantList.view';
