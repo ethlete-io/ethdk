604a173cc544869bc59fb711cb78ba6afa8f5ba4
Marc Gruner <mg@braune-digital.com>
2025-09-23 16:01:31 +0200

    add export for linked

--- libs/cdk/src/lib/components/bracket/components/new-bracket/index.ts
+++ libs/cdk/src/lib/components/bracket/components/new-bracket/index.ts
@@ -1,5 +1,6 @@
 export * from './core';
 export * from './integrations';
+export * from './linked';
 export * from './new-bracket-default-match.component';
 export * from './new-bracket-default-round-header.component';
 export * from './new-bracket.component';
