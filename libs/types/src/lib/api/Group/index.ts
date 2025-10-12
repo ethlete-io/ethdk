67c2dea555969db4dca864578727f7b961ba5a86
Gitlab CI Pipeline <gitlab@braune-digital.com>
2025-10-12 11:35:39 +0000

    dev-vbl-cc-25-26 from API (c1631b1c)

--- libs/types/src/lib/api/Group/index.ts
+++ libs/types/src/lib/api/Group/index.ts
@@ -1,4 +1,4 @@
 export { GroupListViewUnion } from './groupListView.union';
-export { GroupDetailView } from './groupDetail.view';
 export { GroupListView } from './groupList.view';
 export { GroupRankingView } from './groupRanking.view';
+export { GroupDetailView } from './groupDetail.view';
