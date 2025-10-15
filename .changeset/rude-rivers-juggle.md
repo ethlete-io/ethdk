6582807723310479c3b926e011bdfd6ded913564
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2025-10-15 11:11:44 +0200

    feat: add extraInjectionToken option to createProvider utility for additional provider injection

---
+++ .changeset/rude-rivers-juggle.md
@@ -0,0 +1,5 @@
+---
+'@ethlete/cdk': minor
+---
+
+Add `extraInjectionToken` option to `createProvider` util to allow providing the created provider under an additional existing `InjectionToken`. 
--- libs/cdk/src/lib/utils/create-provider.ts
+++ libs/cdk/src/lib/utils/create-provider.ts
@@ -7,15 +7,23 @@ export type CreateProviderOptions = {
    * If not provided, the name will be a generated string.
    */
   name?: string;
+
+  /**
+   * Optional existing InjectionToken to also provide the created provider under.
+   */
+  extraInjectionToken?: InjectionToken<unknown>;
 };
 
 export const createProvider = <T>(factory: () => T, options?: CreateProviderOptions) => {
   const injectionToken = new InjectionToken<T>(options?.name ?? createComponentId('provider'));
 
-  const provide = () => ({
-    provide: injectionToken,
-    useFactory: factory,
-  });
+  const provide = () => [
+    {
+      provide: injectionToken,
+      useFactory: factory,
+    },
+    ...(options?.extraInjectionToken ? [{ provide: options.extraInjectionToken, useExisting: injectionToken }] : []),
+  ];
 
   const inject = () => ngInject(injectionToken);
 
