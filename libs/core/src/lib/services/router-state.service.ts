a5d181cc79dd88f979bbd7564f0acda2eaf6616a
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2025-11-04 20:40:42 +0100

    refactor: reorganize scroll restoration constants and update RouterStateService to use new config type

--- libs/core/src/lib/services/router-state.service.ts
+++ libs/core/src/lib/services/router-state.service.ts
@@ -12,63 +12,13 @@ import {
   shareReplay,
   tap,
 } from 'rxjs';
+import {
+  ET_DISABLE_SCROLL_TOP,
+  ET_DISABLE_SCROLL_TOP_AS_RETURN_ROUTE,
+  ET_DISABLE_SCROLL_TOP_ON_PATH_PARAM_CHANGE,
+  SetupScrollRestorationConfig,
+} from '../signals';
 import { equal } from '../utils';
-
-const ET_DISABLE_SCROLL_TOP = Symbol('ET_DISABLE_SCROLL_TOP');
-const ET_DISABLE_SCROLL_TOP_AS_RETURN_ROUTE = Symbol('ET_DISABLE_SCROLL_TOP_AS_RETURN_ROUTE');
-const ET_DISABLE_SCROLL_TOP_ON_PATH_PARAM_CHANGE = Symbol('ET_DISABLE_SCROLL_TOP_ON_PATH_PARAM_CHANGE');
-
-export interface RouterDisableScrollTopConfig {
-  /**
-   * Whether to disable scroll to top ONLY when navigating back to this route.
-   * @default false
-   */
-  asReturnRoute?: boolean;
-
-  /**
-   * Whether to disable scroll to top when a path param changes.
-   * @default false
-   */
-  onPathParamChange?: boolean;
-}
-
-export const routerDisableScrollTop = (config: RouterDisableScrollTopConfig = {}) => {
-  return {
-    ...(!config.asReturnRoute ? { [ET_DISABLE_SCROLL_TOP]: true } : { [ET_DISABLE_SCROLL_TOP_AS_RETURN_ROUTE]: true }),
-    ...(config.onPathParamChange ? { [ET_DISABLE_SCROLL_TOP_ON_PATH_PARAM_CHANGE]: true } : {}),
-  };
-};
-
-export const ET_PROPERTY_REMOVED = Symbol('ET_PROPERTY_REMOVED');
-
-export interface ScrollEnhancementsConfig {
-  /**
-   * The scrollable container.
-   * @default document.documentElement
-   */
-  scrollElement?: HTMLElement;
-  /**
-   * A list of query params that should trigger a scroll to top.
-   * @default []
-   * @example ['page'] // will scroll to top when the page query param changes
-   */
-  queryParamTriggerList?: string[];
-  /**
-   * Config for fragment scrolling.
-   */
-  fragment?: {
-    /**
-     * Enable fragment scrolling (scroll to element with id)
-     * @default false
-     */
-    enabled?: boolean;
-    /**
-     * Whether to use smooth scrolling or not.
-     * @default false
-     */
-    smooth?: boolean;
-  };
-}
 
 export interface RouterState {
   data: Data;
@@ -77,6 +27,8 @@ export interface RouterState {
   title: string | null;
   fragment: string | null;
 }
+
+export const ET_PROPERTY_REMOVED = Symbol('ET_PROPERTY_REMOVED');
 
 /** @deprecated use respective signal utils instead */
 @Injectable({
@@ -236,7 +188,7 @@ export class RouterStateService {
       .subscribe();
   }
 
-  enableScrollEnhancements(config: ScrollEnhancementsConfig = {}) {
+  enableScrollEnhancements(config: SetupScrollRestorationConfig = {}) {
     if (this._isScrollTopOnNavigationEnabled) {
       return;
     }
