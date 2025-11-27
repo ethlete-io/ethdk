55dc61289eea70158471d82f02bf2a9cfca42cad
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2025-11-27 16:51:14 +0100

    refactor(deps): Move from git-cz to commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert'],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'core',
+        'cdk',
+        'query',
+        'types',
+        'cli',
+        'contentful',
+        'eslint-plugin',
+        'playground',
+        'docs',
+        'deps',
+        'ci',
+        'release',
+      ],
+    ],
+    'scope-empty': [2, 'never'], // Require scope
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
+};
