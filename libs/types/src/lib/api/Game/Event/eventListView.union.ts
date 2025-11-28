5b1b67c7a36494ca98c172760de32a5930714349
Gitlab CI Pipeline <gitlab@braune-digital.com>
2025-11-28 17:14:32 +0000

    dev-fifagg from API (4a418f97)

--- libs/types/src/lib/api/Game/Event/eventListView.union.ts
+++ libs/types/src/lib/api/Game/Event/eventListView.union.ts
@@ -1,6 +1,6 @@
 // Generated Type, do not change manually!
 import { EventListView } from './../../Game/Event/eventList.view';
-import { PlayerEventView } from './../../Game/Event/playerEvent.view';
 import { SubstitutionEventView } from './../../Game/Event/substitutionEvent.view';
+import { PlayerEventView } from './../../Game/Event/playerEvent.view';
 
-export type EventListViewUnion = EventListView | PlayerEventView | SubstitutionEventView;
+export type EventListViewUnion = EventListView | SubstitutionEventView | PlayerEventView;
