dd92bef96c61515a055863065066c337854107f2
Gitlab CI Pipeline <gitlab@braune-digital.com>
2026-01-21 09:36:17 +0000

    dev-vbl-statistic from API (a9754411)

--- libs/types/src/lib/api/Match/index.ts
+++ libs/types/src/lib/api/Match/index.ts
@@ -1,5 +1,7 @@
 export { MatchListViewUnion } from './matchListView.union';
-export { ListUpcomingMatchesForUserSearchRequestData } from './listUpcomingMatchesForUserSearchRequest.data';
-export { DetailedMatchListView } from './detailedMatchList.view';
 export { MatchRankingView } from './matchRanking.view';
 export { MatchListView } from './matchList.view';
+export { DetailedMatchListView } from './detailedMatchList.view';
+export { ListUpcomingMatchesForUserSearchRequestData } from './listUpcomingMatchesForUserSearchRequest.data';
+export { IdentityMatchResultView } from './identityMatchResult.view';
+export { LastXMatchesByIdentityView } from './lastXMatchesByIdentity.view';
--- libs/types/src/lib/api/Statistic/HeadToHead/index.ts
+++ libs/types/src/lib/api/Statistic/HeadToHead/index.ts
@@ -1,2 +1,2 @@
-export { StatisticPlayerView } from './statisticPlayer.view';
 export { StatisticClubView } from './statisticClub.view';
+export { StatisticPlayerView } from './statisticPlayer.view';
