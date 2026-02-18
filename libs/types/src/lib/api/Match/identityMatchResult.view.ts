e49b07c40aa50cf3b56e408a897c1b31d02957dd
Gitlab CI Pipeline <gitlab@braune-digital.com>
2026-02-18 10:55:38 +0000

    dev-vbl-statistic from API (7d23e4a9)

---
+++ libs/types/src/lib/api/Match/identityMatchResult.view.ts
@@ -0,0 +1,11 @@
+// Generated Interface, do not change manually!
+
+export interface IdentityMatchResultView {
+    matchId: string;
+    playedAt: string;
+    participantScore: number;
+    opponentScore: number;
+    result: string;
+}
+
+export default IdentityMatchResultView;
