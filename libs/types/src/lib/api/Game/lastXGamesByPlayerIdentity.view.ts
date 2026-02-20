b2b02c2abb5ceeb0af378524cd8f33a89e19aedf
Gitlab CI Pipeline <gitlab@braune-digital.com>
2026-02-20 12:16:15 +0000

    dev-vbl-statistic from API (4ecefae3)

---
+++ libs/types/src/lib/api/Game/lastXGamesByPlayerIdentity.view.ts
@@ -0,0 +1,8 @@
+// Generated Interface, do not change manually!
+
+export interface LastXGamesByPlayerIdentityView {
+    playerIdentity: string;
+    results: any;
+}
+
+export default LastXGamesByPlayerIdentityView;
