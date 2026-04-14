2b2d8e796f5cc779af5d126a4f707da3564ef039
Gitlab CI Pipeline <gitlab@braune-digital.com>
2026-04-14 10:36:17 +0000

    dev-vbl-statistic from API (99909e54)

--- libs/types/src/lib/api/Weekly/statisticWeeklyUtCardResults.view.ts
+++ libs/types/src/lib/api/Weekly/statisticWeeklyUtCardResults.view.ts
@@ -1,5 +1,5 @@
 // Generated Interface, do not change manually!
-import { MediaMinimalView } from '../Media';
+import { MediaMinimalView } from './mediaMinimal.view';
 
 export interface StatisticWeeklyUtCardResultsView {
     goalsShot: number | null;
