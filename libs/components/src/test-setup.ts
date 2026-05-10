2ec65aa371f597a2b8d32bb0d1e31e7b53819fd7
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2026-05-10 23:33:21 +0200

    fix(components): Lint error

--- libs/components/src/test-setup.ts
+++ libs/components/src/test-setup.ts
@@ -1,6 +1,5 @@
 import '@analogjs/vitest-angular/setup-zone';
 import '@angular/compiler';
-
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
