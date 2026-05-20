302553523f07d6def249eba60270488a19d2eb1f
Gitlab CI Pipeline <gitlab@braune-digital.com>
2026-05-20 13:53:02 +0000

    dev-fifagg from API (0ed41101)

--- libs/types/src/lib/api/Participant/playerParticipant.view.ts
+++ libs/types/src/lib/api/Participant/playerParticipant.view.ts
@@ -1,11 +1,7 @@
 // Generated Interface, do not change manually!
 import { AbstractParticipantView } from './abstractParticipant.view';
-import { ClubListView } from './../Club/clubList.view';
 
 export interface PlayerParticipantView extends AbstractParticipantView {
-    gamertag: string | null;
-    club: ClubListView | null;
-    mediaCollection: any[];
 }
 
 export default PlayerParticipantView;
