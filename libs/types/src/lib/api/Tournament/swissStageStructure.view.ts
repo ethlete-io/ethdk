c25c3c7d572e2d0ef7b674694cc7934c49be979d
Gitlab CI Pipeline <gitlab@braune-digital.com>
2026-06-22 20:12:24 +0000

    dev-fifagg from API (cc180f20)

--- libs/types/src/lib/api/Tournament/swissStageStructure.view.ts
+++
@@ -1,9 +0,0 @@
-// Generated Interface, do not change manually!
-import { BaseStageStageStructureView } from './baseStageStageStructure.view';
-import { RoundStageStructureView } from './roundStageStructure.view';
-
-export interface SwissStageStructureView extends BaseStageStageStructureView {
-    rounds: RoundStageStructureView[];
-}
-
-export default SwissStageStructureView;
